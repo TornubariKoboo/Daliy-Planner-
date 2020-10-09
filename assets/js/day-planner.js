0c956b48eb8c20ff37c681e0a9f1ee1f505a5761
Tornubari <t.koboo12@gmail.com>
2020-10-09 19:43:48 -0400

    added clock to current Day id

--- assets/js/day-planner.js
+++ assets/js/day-planner.js
@@ -4,3 +4,9 @@ var availableHours = {};
 var m = moment();
 var newDay = moment().hour(0);
 var currentTime = m.hour();
+
+// adding clock to currentDay id
+function clock() {
+  var dateString = moment().format('MMMM Do YYYY, h:mm:ss a');
+  $('#currentDay').html(dateString);
+}
